0c6139b51362c7f9e8e295520ece26ebdd76ee20
Anton Yaskov <yaskovanton333@gmail.com>
2024-10-05 23:29:09 +0400

    CacheInMemory

---
+++ API/API/Modules/CacheModule/CacheInMemory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+
+namespace API.Modules.CacheModule;
+
+public class CacheInMemory : ICache
+{
+    private readonly ConcurrentDictionary<string, string> inner;
+
+    public CacheInMemory()
+    {
+        inner = new ConcurrentDictionary<string, string>();
+    }
+
+
+    public string? Get(string key)
+    {
+        if (inner.TryGetValue(key, out var value))
+            return value;
+        return null;
+    }
+
+    public void Add(string key, string value)
+    {
+        if (inner.ContainsKey(key))
+            throw new Exception($"Cache already have same key: {key}");
+
+        if (!inner.TryAdd(key, value))
+            throw new Exception($"Can not set value to cache. key {key}, value {value}");
+    }
+
+    public void Delete(string key)
+    {
+        inner.Remove(key, out var value);
+    }
+}
---
+++ API/API/Modules/CacheModule/ICache.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Modules.CacheModule;
+
+public interface ICache
+{
+    public string? Get(string key);
+    public void Add(string key, string value);
+    public void Delete(string key);
+}
