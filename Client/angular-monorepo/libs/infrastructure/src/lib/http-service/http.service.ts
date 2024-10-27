cf40794baadb24407da03973d05725717b317999
YEgorLu <99907142+YEgorLu@users.noreply.github.com>
2024-10-28 03:07:52 +0500

    lk: change server url

--- Client/angular-monorepo/libs/infrastructure/src/lib/http-service/http.service.ts
+++ Client/angular-monorepo/libs/infrastructure/src/lib/http-service/http.service.ts
@@ -6,26 +6,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class HttpService {
 
+  BACKEND_URL = 'http://www.crimean-billing.work.gd/api/'
+
   private httpClient = inject(HttpClient);
   private origin = location.origin;
 
   public get<T>(method: string) {
-    return this.httpClient.get<T>(`${this.origin}/api/${method}`);
+    return this.httpClient.get<T>(`${this.BACKEND_URL}${method}`);
   }
 
   public post<T>(method: string, body?: any, options?: { headers: HttpHeaders } | {[p: string] : string }) {
-    return this.httpClient.post<T>(`${this.origin}/api/${method}`, body, options);
+    return this.httpClient.post<T>(`${this.BACKEND_URL}${method}`, body, options);
   }
 
   public put(method: string, body: any, options?: { headers: HttpHeaders | {[p: string] : string} }) {
-    return this.httpClient.put(`${this.origin}/api/${method}`, body, options);
+    return this.httpClient.put(`${this.BACKEND_URL}${method}`, body, options);
   }
 
   public patch(method: string, body: any, options?: { headers: HttpHeaders | {[p: string] : string} }) {
-    return this.httpClient.patch(`${this.origin}/api/${method}`, body, options);
+    return this.httpClient.patch(`${this.BACKEND_URL}${method}`, body, options);
   }
 
   public delete(method: string, options?: { headers: HttpHeaders | {[p: string] : string} }) {
-    return this.httpClient.delete(`${this.origin}/api/${method}`, options);
+    return this.httpClient.delete(`${this.BACKEND_URL}${method}`, options);
   }
 }
