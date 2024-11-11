eab7f37230e38d97764a49c33ac926a3ed0e9f27
YEgorLu <99907142+YEgorLu@users.noreply.github.com>
2024-11-11 21:48:30 +0500

    update backend url

--- Client/angular-monorepo/libs/infrastructure/src/lib/http-service/http.service.ts
+++ Client/angular-monorepo/libs/infrastructure/src/lib/http-service/http.service.ts
@@ -13,7 +13,7 @@ export class HttpService {
   private httpClient = inject(HttpClient);
 
   private origin = location.origin;
-  BACKEND_URL = `${getProtocol()}://www.crimean-billing.work.gd/api/`
+  BACKEND_URL = `${getProtocol()}://crimean-billing.work.gd/api/`
 
   public get<T>(method: string) {
     return this.httpClient.get<T>(`${this.BACKEND_URL}${method}`);
