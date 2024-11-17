6700a49e419e76b4edd75f1620b07bb30a8fb1d1
YEgorLu <99907142+YEgorLu@users.noreply.github.com>
2024-11-18 00:14:51 +0500

    main-router routes

--- Client/angular-monorepo/apps/manager-app/src/app/modules/main-router/main-router.component.ts
+++ Client/angular-monorepo/apps/manager-app/src/app/modules/main-router/main-router.component.ts
@@ -5,23 +5,6 @@ import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
 
-const NAVIGATOR_ROUTES_VISUALS = {
-  'services': 'Услуги',
-  'managers': 'Менеджеры',
-  'invoices': 'Счета',
-  'payments': 'Оплаты',
-  'abonents': 'Абоненты',
-  'accounts': 'Лицевые счета'
-}
-
-const ROUTE_NAMES = [
-  'abonents',
-  'accounts',
-  'payments',
-  'invoices',
-  'services',
-  'managers',
-]
 
 @Component({
   selector: 'app-main',
@@ -38,9 +21,6 @@ const ROUTE_NAMES = [
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class MainComponent {
-
-  navigatorRoutes = ROUTE_NAMES
-  navigatorRoutesVisuals = NAVIGATOR_ROUTES_VISUALS as Record<typeof ROUTE_NAMES[number], string>
 
 
 }
