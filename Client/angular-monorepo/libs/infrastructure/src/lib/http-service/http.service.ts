2befe5271bdad3cad11815348423916159674fbc
Nikita Osminin <nikita.karpinskiy@mail.ru>
2025-01-21 22:07:34 +0300

    изменение протокола v2

--- Client/angular-monorepo/libs/infrastructure/src/lib/http-service/http.service.ts
+++ Client/angular-monorepo/libs/infrastructure/src/lib/http-service/http.service.ts
@@ -10,7 +10,7 @@ export class HttpService {
   private httpClient = inject(HttpClient);
   private origin = location.origin;
 
-  BACKEND_URL =  isDevMode() ? this.origin + '/api/' : `https://crimean-billing.work.gd/`;
+  BACKEND_URL =  isDevMode() ? this.origin + '/api/' : `https://crimean-billing.work.gd/api/`;
 
   public get<T>(method: string) {
     return this.httpClient.get<T>(`${this.BACKEND_URL}${method}`, { withCredentials: true });
