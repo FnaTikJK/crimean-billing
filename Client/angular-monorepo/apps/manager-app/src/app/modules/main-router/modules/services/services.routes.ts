5c3705f65d82c6758bfec5b8a99baca5f08f1e20
YEgorLu <99907142+YEgorLu@users.noreply.github.com>
2025-01-22 17:17:58 +0500

    fix: remove create from services

--- Client/angular-monorepo/apps/manager-app/src/app/modules/main-router/modules/services/services.routes.ts
+++ Client/angular-monorepo/apps/manager-app/src/app/modules/main-router/modules/services/services.routes.ts
@@ -1,10 +1,10 @@
 import { Route } from '@angular/router';
 
 export default [
-  {
-    path: 'create',
-    loadComponent: () => import('./modules/create/service-create.component')
-  },
+  // {
+  //   path: 'create',
+  //   loadComponent: () => import('./modules/create/service-create.component')
+  // },
   {
     path: ':id',
     loadComponent: () => import('./services.component')
